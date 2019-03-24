73cf64adbd34c6e3bfec20b59a41011e27f6470a
Ojoniko <oabah@users.noreply.github.com>
2019-03-23 20:04:42 -0500

    Tidying data

--- CyberBullying_Analysis_Rcode.R
+++ CyberBullying_Analysis_Rcode.R
@@ -1,22 +1,26 @@
 #This example requires xml2 R package
 require(xml2)
 
-#Parse XML from an online document found at the URL specified below
+#Parse XML
 URL <- "C:/Users/Ojoniko Nathan Abah/Desktop/CyberBullying_RProject/rProject_repo/cyberbullying_dataset/DataReleaseDec2011/XMLMergedFile.xml"
 XML_data <- read_xml(URL)
 
-#Save all nodes related to Net Income or Loss as defined by US GAAP
-Nodes <- xml_find_all(XML_data, "//CYBERBULLYWORD")
+#retrieve text case
+Nodes_0 <- xml_find_all(XML_data, "//TEXT")
+cases <- as.character(Nodes_0)
 
-#Convert the node structure into a character vector
-bullyword <- as.character(Nodes)
+#retrieve answer
+Nodes_1 <- xml_find_all(XML_data, "//ANSWER")
+bully_or_not <- as.character(Nodes_1)
 
-#Retreive values for all Net Income or Loss elements and save them into a vetcor
-#These are dollar values for each Net Income or Loss item reported in the 10Q document
-Nodes_Values <- xml_find_all(XML_data, "//SEVERITY")
-severity <- as.character(Nodes_Values)
+#retrieve bullyword
+Nodes_2 <- xml_find_all(XML_data, "//CYBERBULLYWORD")
+bullyword <- as.character(Nodes_2)
+
+#Retreive severity
+Nodes_3 <- xml_find_all(XML_data, "//SEVERITY")
+severity <- as.character(Nodes_3)
 
 #Bind the vectors together as columns and convert the structure into a data frame
-#The data frame contains two columns and four rows
-LABElData <- data.frame(cbind(bullyword,severity))
+LABElData <- data.frame(cbind(cases,bully_or_not,bullyword,severity))
 
